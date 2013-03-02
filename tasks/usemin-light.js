eafb1f8d77054543f81b5c102edc93db510ab808
Ian W. Remmel <design@ianwremmel.com>
2013-03-02 00:29:04 -0500

    Got file reading working, fixed a typo

--- tasks/usemin-light.js
+++ tasks/usemin-light.js
@@ -3,19 +3,29 @@
 module.exports = function (grunt) {
 	var processor = require('../lib/processor');
 
-	grunt.registerMultiTask('usemin', 'Replaces references to assets', function () {
-		this.files.forEach(function (fileObj) {
-			var files = grunt.file.expand({nonull: true}, fileObj.src);
-			files.map(grunt.file.read).forEach(function (content, i) {
-				var filepath = files[i];
+	grunt.registerMultiTask('useminLight', 'Replaces references to assets', function () {
+
+		this.files.forEach(function (file) {
+
+			file.src.filter(function(filepath) {
+				// Remove nonexistent files (it's up to you to filter or warn here).
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			})
+			.map(function(filepath) {
+				var content = grunt.file.read(filepath);
 
 				// make sure to convert back into utf8, `file.read` when used as a
-        // forEach handler will take additional arguments, and thus trigger the
-        // raw buffer read
+	      // forEach handler will take additional arguments, and thus trigger the
+	      // raw buffer read
 				content = content.toString();
-				content = processor.process;
+				content = processor.process(content);
 
-				grunt.file.write(filepath, content);
+				grunt.file.write(file.dest, content);
 			});
 		});
 	});
